531d5d8eca530068d4f7c96ace65063e6b103dff
mdutralopes <mdutra.lopes@gmail.com>
2024-11-08 08:01:43 -0300

    Segundo commit

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá galera!");
+        System.out.println("Olá galera! Teste");
     }
 }
